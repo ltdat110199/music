9b7c953207927782acfca29c9686ca999e40af28
Lê Thành Đạt <ltdat110199@gmail.com>
2024-08-26 14:35:30 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create(AppModule);
 
-  app.useStaticAssets(join(__dirname, '..', 'uploads'));
   await app.listen(3000);
 }
 bootstrap();
